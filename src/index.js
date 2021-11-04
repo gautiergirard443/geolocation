0b19228cc24b01f5d3905e6a63a411e28be0c2be
Gautier <gautiergirard444@gmail.com>
2021-11-04 13:15:18 +0100

    Fin refactoring + attente utilisation React State System

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+/* const App = () => {
 
 window.navigator.geolocation.getCurrentPosition(
     (position) => console.log(position),
@@ -13,9 +13,23 @@ window.navigator.geolocation.getCurrentPosition(
       Hello world!
       </div>
   )
-}
+} */
 
 // Ne peut pas fonctionner de manière efficace => Functionnal component can't get the position and render it in the same time. => Need to refactor it to get a class-based component (Voir fiches post it exo Geoloc.)
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error) 
+        );
+        return (
+            <div>
+                You are in the northern hemishpere.
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
     <App />,
