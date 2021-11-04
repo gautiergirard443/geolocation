1082a17e7a2c69229bb48a5945e39e560eabe69b
Gautier <gautiergirard444@gmail.com>
2021-11-04 12:46:37 +0100

    commit

---
+++ src/HemishpereDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HemishpereDisplay = () => {
+return(
+    <div>
+        Hey! You are in Northern Hemisphere!
+    </div>
+)
+
+}
+
+export default HemishpereDisplay
