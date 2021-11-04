b3ff79a518016e74db3cd301d341a71d0d27e496
Gautier <gautiergirard444@gmail.com>
2021-11-04 14:38:21 +0100

    Initialisation du State + constructeur

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 /* const App = () => {
 
@@ -18,20 +18,19 @@ window.navigator.geolocation.getCurrentPosition(
 // Ne peut pas fonctionner de manière efficace => Functionnal component can't get the position and render it in the same time. => Need to refactor it to get a class-based component (Voir fiches post it exo Geoloc.)
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (error) => console.log(error) 
-        );
-        return (
-            <div>
-                You are in the northern hemishpere.
-            </div>
-        )
-    }
+    
+  constructor(props) {
+    super(props);
+    this.state = { latitude: 25 };
+  }
+
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.log(error)
+    );
+    return <div>{this.state.latitude}</div>;
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
